56c8dca49ecfe38a17ba7b54827abfa49e967cd2
ShadmanShihab <shadmanshihab0@gmail.com>
2021-03-17 00:41:44 +0600

    home login added

--- src/main/java/com/registration/userportal/controller/LoginController.java
+++ src/main/java/com/registration/userportal/controller/LoginController.java
@@ -11,6 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
+    @RequestMapping("/")
+    public String home(Model m){
+        //m.addAttribute("login_message", "");
+        return "login";
+    }
+
 
     @RequestMapping("/login")
     public String login(Model m){
